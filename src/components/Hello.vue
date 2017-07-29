97ec511d438895e2efabc8a2c367d29dd19443c0
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2017-07-29 13:36:04 -0500

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="hello">
+
+  <section v-scroll-reveal>
+    <h1>Scroll down!</h1>
+  </section>
+
+  <section v-scroll-reveal>
+    <h1>Tada!</h1>
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  section {
+    height: 100vh;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,35 @@
+// Absolute Imports
+import vueResource from 'vue-resource';
+import BootstrapVue from 'bootstrap-vue';
+import Vuelidate from 'vuelidate';
+import VueAnalytics from 'vue-analytics';
+import VueScrollReveal from 'vue-scroll-reveal';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Relative Imports
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+// Dependendies Setup
+Vue.use(vueResource);
+Vue.use(BootstrapVue);
+Vue.use(VueScrollReveal);
+Vue.use(Vuelidate);
+Vue.use(VueAnalytics, {
+  id: 'UA-XXX-X',
+});
+Vue.use(require('vue-moment'));
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+});
